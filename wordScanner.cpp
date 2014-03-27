4a6b08ed5dda744cd06234c89627abb044e56d05
dcatz <dcatz@katzchen>
2014-03-27 15:32:28 -0700

    first command

---
+++ wordScanner.cpp
@@ -0,0 +1,16 @@
+/*wordScanner.cpp
+ * 
+ */
+ #include "wordScanner.h"
+ 
+ using namespace std;
+ 
+ wordScanner::wordScanner()
+ {
+	 word = " ";
+ }
+ 
+ bool wordScanner::scanMe(const &string scanWord)
+ {
+	 
+ }
---
+++ wordScannerMain.cpp
@@ -0,0 +1,15 @@
+/*wordScannerMain.cpp
+ */
+ 
+ #include "wordScanner.h"
+ 
+ using namespace std;
+ 
+ int main()
+ {
+	 string userInput;
+	 
+	 cin >> userInput;
+	 
+	 
+ }
