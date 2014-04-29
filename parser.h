19cd99d31d79c9599ffedc4dbd0d4091c3ca86a6
nomoreclowns <sosar001@cougars.csusm.edu>
2014-04-28 22:31:13 -0700

    .

---
+++ parser.h
@@ -0,0 +1,14 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+
+class Parser
+{
+    public:
+        Parser();
+        virtual ~Parser();
+    protected:
+    private:
+};
+
+#endif // PARSER_H
--- wordTestMain.cpp
+++ wordTestMain.cpp
@@ -1,53 +1,53 @@
 /*test for wordScanner and wordDfa
  * */
- 
+
+ #include <fstream>
+ #include <string>
+ using namespace std;
+
  #include "wordDfa.h"
  #include "lexical.h"
  #include "pDfa.h"
- #include <fstream>
- #include <string>
- 
- using namespace std;
- 
+
  int main()
  {
 	 ifstream readMe;
-	 
+
 	 wordDfa dfa;
 	 lexical lex;
 	 pDfa period;
 	 string userInput = " ";
-	 
+
 	 string testMe = " ";
-	 
+
 	 cout << "name of file: \n";
 	 cin >> userInput;
-	 
+
 	 /*file reader
 	  * opens and reads the words into the scanner
 	  * */
-	
+
 	readMe.open(userInput.c_str());
-	 
+
 	 if(readMe.is_open())
 	 {
 		while(!readMe.eof())
 		{
-			
+
 			readMe >> testMe;
-			
-			/*if the word is true 
+
+			/*if the word is true
 			* pass to a function that first checks for the reserved words
 			* the check to see if it is in the dictionary (lexicon). if it
-			* is not in the dictionary already, then it adds the new word 
+			* is not in the dictionary already, then it adds the new word
 			* into the dictionary.
 			* */
-	 
+
 			if(dfa.scanMe(testMe))
 			{
-				
+
 				lex.checkReserve(testMe);
-				
+
 				//cout << "yup!!!\n\n";
 			}
 			else
@@ -60,15 +60,15 @@
 				{
 					cout << "Lexical Error!!!\n";
 				}
-				
+
 			}
-			
-		}//end of while(!readMe.eof())	 
+
+		}//end of while(!readMe.eof())
 	 }
 	 else
 	 {
 		 cout << "file could not be read..." << endl;
 	 }
-	 
+
 		return 0;
  }
