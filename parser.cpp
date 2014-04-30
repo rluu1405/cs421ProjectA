dda0d37415a0ae906f93914b30214823635cf234
nomoreclowns <sosar001@cougars.csusm.edu>
2014-04-29 19:25:44 -0700

    Slowly adding in the parser

--- parser.cpp
+++ parser.cpp
@@ -1,7 +1,25 @@
 #include "parser.h"
+#include <string>
+#include <stack>
+using namespace std;
+
+TokenObject::TokenObject()
+{
+    Word = "";
+    Token = "";
+}
+
+TokenObject::TokenObject(string word, string type)
+{
+    Word = word;
+    Token = type;
+}
 
 Parser::Parser()
 {
+    stack<TokenObject> dummy;
+
+    contents = dummy;
     //ctor
 }
 
@@ -9,3 +27,20 @@ Parser::~Parser()
 {
     //dtor
 }
+
+void Parser::AddToken(TokenObject Content)
+{
+    contents.push(Content);
+}
+
+void Parser::AddToken(string word, string type)
+{
+    TokenObject dummy(word, type);
+    AddToken(dummy);
+}
+
+bool Parser::Process()
+{
+    ;
+}
+
--- parser.h
+++ parser.h
@@ -1,14 +1,30 @@
 #ifndef PARSER_H
 #define PARSER_H
+#include <string>
+#include <stack>
+using namespace std;
 
+class TokenObject
+{
+public:
+    TokenObject();
+    TokenObject(string, string);
+    string Word;
+    string Token;
+};
 
 class Parser
 {
     public:
         Parser();
         virtual ~Parser();
+        void AddToken(TokenObject);
+        void AddToken(string, string);
+        bool Process();
     protected:
+        stack<TokenObject> contents;
     private:
+
 };
 
 #endif // PARSER_H
